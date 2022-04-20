29a532ebb38c0f81e6c80cd44ac6f1e0a9f212c0
JirusAnun <hamjirus@gmail.com>
2022-04-20 14:21:46 +0700

    Design login page

--- Project1/MainForm.h
+++ Project1/MainForm.h
@@ -37,14 +37,16 @@ namespace Project1 {
 		}
 
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Label^ label1;
+
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
+
+
 	private: System::Windows::Forms::CheckBox^ checkBox1;
 	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Button^ button3;
+
+
 
 
 	private: System::ComponentModel::IContainer^ components;
@@ -66,14 +68,11 @@ namespace Project1 {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MainForm::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -90,101 +89,87 @@ namespace Project1 {
 			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
 			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 30));
-			this->label1->Location = System::Drawing::Point(825, 77);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(194, 58);
-			this->label1->TabIndex = 2;
-			this->label1->Text = L"Sign up";
-			this->label1->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
-			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(700, 226);
+			this->textBox1->BackColor = System::Drawing::SystemColors::MenuText;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
+			this->textBox1->ForeColor = System::Drawing::SystemColors::ScrollBar;
+			this->textBox1->Location = System::Drawing::Point(659, 252);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(403, 38);
+			this->textBox1->Size = System::Drawing::Size(307, 23);
 			this->textBox1->TabIndex = 3;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(700, 342);
+			this->textBox2->BackColor = System::Drawing::SystemColors::MenuText;
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
+			this->textBox2->ForeColor = System::Drawing::SystemColors::ScrollBar;
+			this->textBox2->Location = System::Drawing::Point(659, 357);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(403, 38);
+			this->textBox2->Size = System::Drawing::Size(307, 23);
 			this->textBox2->TabIndex = 4;
 			this->textBox2->UseSystemPasswordChar = true;
 			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MainForm::textBox2_TextChanged);
 			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16));
-			this->label2->Location = System::Drawing::Point(652, 169);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(146, 31);
-			this->label2->TabIndex = 5;
-			this->label2->Text = L"User name";
-			this->label2->Click += gcnew System::EventHandler(this, &MainForm::label2_Click);
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16));
-			this->label3->Location = System::Drawing::Point(652, 288);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(134, 31);
-			this->label3->TabIndex = 6;
-			this->label3->Text = L"Password";
-			// 
 			// checkBox1
 			// 
 			this->checkBox1->AutoSize = true;
-			this->checkBox1->Location = System::Drawing::Point(658, 422);
+			this->checkBox1->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->checkBox1->Location = System::Drawing::Point(659, 427);
 			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(231, 35);
+			this->checkBox1->Size = System::Drawing::Size(18, 17);
 			this->checkBox1->TabIndex = 7;
-			this->checkBox1->Text = L"Show Password";
-			this->checkBox1->UseVisualStyleBackColor = true;
+			this->checkBox1->UseVisualStyleBackColor = false;
 			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &MainForm::checkBox1_CheckedChanged);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(788, 519);
+			this->button2->BackColor = System::Drawing::Color::Navy;
+			this->button2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button2.BackgroundImage")));
+			this->button2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Agency FB", 12));
+			this->button2->ForeColor = System::Drawing::SystemColors::ScrollBar;
+			this->button2->Location = System::Drawing::Point(647, 497);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(231, 64);
+			this->button2->Size = System::Drawing::Size(95, 34);
 			this->button2->TabIndex = 8;
-			this->button2->Text = L"button2";
-			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Text = L"Login";
+			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &MainForm::button2_Click);
 			// 
-			// comboBox1
+			// button3
 			// 
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"Asdfsdfsdfsdf", L"Vsdf", L"Cdsf", L"Ssdf" });
-			this->comboBox1->Location = System::Drawing::Point(172, 251);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(121, 39);
-			this->comboBox1->TabIndex = 9;
-			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MainForm::comboBox1_SelectedIndexChanged);
+			this->button3->BackColor = System::Drawing::Color::Black;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Agency FB", 12));
+			this->button3->ForeColor = System::Drawing::SystemColors::Highlight;
+			this->button3->Location = System::Drawing::Point(748, 497);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(95, 34);
+			this->button3->TabIndex = 9;
+			this->button3->Text = L"Sign up";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &MainForm::button3_Click);
 			// 
 			// MainForm
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->BackColor = System::Drawing::SystemColors::ActiveBorder;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
-			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
 			this->ClientSize = System::Drawing::Size(1280, 720);
-			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->checkBox1);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
-			this->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16));
+			this->Cursor = System::Windows::Forms::Cursors::Default;
+			this->Font = (gcnew System::Drawing::Font(L"Agency FB", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"MainForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
@@ -223,7 +208,6 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 	{
 		MessageBox::Show("Signed in");
 	}
-	comboBox1->Items->Add("test");
 
 }
 	 //drag form
@@ -244,9 +228,7 @@ private: System::Void MainForm_MouseMove(System::Object^ sender, System::Windows
 private: System::Void MainForm_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
 	drag = false;
 }
-private: System::Void domainUpDown1_SelectedItemChanged(System::Object^ sender, System::EventArgs^ e) {
-}
-private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
